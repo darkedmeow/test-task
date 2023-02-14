e451b33aa4ad831d6eea54dd4e85f8248a2f4394
darkedmeow <smorf.2605@gmail.com>
2023-02-14 20:40:18 +0300

    user controller - ready

--- src/main/java/ru/smallgroup/testtask/controllers/UserController.java
+++ src/main/java/ru/smallgroup/testtask/controllers/UserController.java
@@ -1,8 +1,12 @@
 package ru.smallgroup.testtask.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import ru.smallgroup.testtask.models.User;
 import ru.smallgroup.testtask.services.UserService;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/users")
@@ -14,6 +18,30 @@ public class UserController {
         this.userService = userService;
     }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<?> showUser(@PathVariable("id") Long id) {
+        final Optional<User> user = userService.getUserById(id);
+        return user.isPresent()
+                ? new ResponseEntity<>(user, HttpStatus.OK)
+                : new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
 
+    @PostMapping
+    public ResponseEntity<?> create(User user) {
+        final User createdUser = userService.createUser(user);
+        return new ResponseEntity<>(user, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> update(@PathVariable("id") Long id, User user) {
+        final User updatedUser = userService.updateUser(user, id);
+        return new ResponseEntity<>(user, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> delete(@PathVariable("id") Long id) {
+        userService.deleteUser(id);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
 
 }
